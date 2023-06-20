9cb6b3c71f182e92308ef3f2a3a3179fcfcb6724
LiBr <liborui0609@foxmail.com>
2023-06-20 17:24:02 +0800

    little fixes

---
+++ app/not-found.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+export default function ntfpage () {
+  return (
+    <div className='container'>
+      <div className='mt-4 flex items-center justify-center'>
+        <div>
+          <p className='text-5xl tracking-wider mb-3 h-14'>404 NOT FOUND</p>
+          <p className='font-bold'>你似乎来到了不存在的地方。(〃＞目＜)</p>
+          <p>
+          点击
+          <Link href='/' className='hover:text-sky-700'>
+            <ruby>
+              <rb>这里</rb>
+              <rt>/index</rt>
+            </ruby>
+          </Link>
+          返回主页
+        </p>
+        </div>
+        {/* 返回键 */}
+        
+      </div>
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -34,7 +34,7 @@ export default function Home () {
               <Link
                 target='_blank'
                 rel='noopener noreferrer'
-                href='/'
+                href='blog.nvme0n1p.dev'
                 className='hover:text-sky-700'
               >
                 <ruby>
